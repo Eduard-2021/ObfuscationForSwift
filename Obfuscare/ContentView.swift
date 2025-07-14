1c84c8c35a911f98b3d2933eab11abff5b0112a5
Eduard <sergsim1978@gmail.com>
2025-07-14 13:50:37 +0300

    Initial Commit

---
+++ Obfuscare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Obfuscare
+//
+//  Created by Macintosh HD on 14.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Obfuscare/ObfuscareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObfuscareApp.swift
+//  Obfuscare
+//
+//  Created by Macintosh HD on 14.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ObfuscareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
